8ec374eccd9dd0f80740d51360583df8b8f86ef3
Alchemist <95372166+ImmortalAlchemist@users.noreply.github.com>
2025-11-15 04:30:28 -0800

    v0.6.1
    made movement work

--- Week6/Midterm/js/script.js
+++ Week6/Midterm/js/script.js
@@ -1,50 +1,37 @@
-let canvas = document.getElementById("interactive-portfolio");
-let ctx = canvas.getContext("2d");
+//referem=nce variables
+const canvas = document.getElementById("interactive-portfolio");
+const ctx = canvas.getContext("2d");
+const image = document.getElementById("icon");
+const inputlog = document.getElementById("last-input");
 
 //movement vars
 let iconX = 0
 let iconY = 0
 
-const image = document.getElementById("icon");
+//parameters
+const speed = 2 //how fast in integers the sprite moves on screen
+let iconr = 16 //the radius of the icon. could change based on screen size
+
+
 
 //when DOM is loaded
 document.addEventListener("DOMContentLoaded", (event) => {
     getMostRecentGames();
 
-    //if you find input
+    //if you find the canvas
     if (canvas != null) {
-        //input
-        document.addEventListener("keypress", (event) => {
-        if (event.key === "ArrowUp") {
-            iconY -= 1;
-            //draw icon
-            ctx.drawImage(image, iconX, IconY);
-            console.log("ArrowUp");
-        } else if (event.key === "ArrowDown") {
-            iconY += 1;
-            //draw icon
-            ctx.drawImage(image, iconX, IconY);
-            console.log("ArrowDown");
-        } else if (event.key === "ArrowLeft") {
-            iconX -= 1;
-            //draw icon
-            ctx.drawImage(image, iconX, IconY);
-            console.log("ArrowLeft");
-        } else if (event.key === "ArrowRight") {
-            iconX += 1;
-            //draw icon
-            ctx.drawImage(image, iconX, IconY);
-            console.log("ArrowRight");
-        }});
+        //input, only detects last currently held down/pressed key
+        document.addEventListener("keydown", (event) => {
+            moveIcon(event.key);
+        });            
     }
-});
 
-//when page is fully loaded
-image.addEventListener("load", (event) => {
+    //generate the border at the edge of the canvas
+    generateBorder();
+
     //draw icon
-    ctx.drawImage(image, iconX, IconY);
+    drawIcon(image);
 });
-
 
 
 async function getMostRecentGames() {
@@ -68,4 +55,62 @@ async function getMostRecentGames() {
         result.textContent = `${error}\n\nThis is probably because the site uses cloudflare`;
     });
 }
-  
+
+function generateBorder() {
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 4;
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+}
+
+function moveIcon(key) {
+    switch (key) {
+        case "ArrowUp":
+            iconY -= 1 * speed;
+            
+            drawIcon(image);
+
+            //test feedback
+            inputlog.innerText = "Last Input: ArrowUp";
+            console.log("ArrowUp");
+            break;
+        case "ArrowDown":
+            iconY += 1 * speed;
+            
+            drawIcon(image);
+            //test feedback
+            inputlog.innerText = "Last Input: ArrowDown";
+            console.log("ArrowDown");
+            break;
+        case "ArrowLeft":
+            iconX -= 1 * speed;
+            
+            drawIcon(image);
+            //test feedback
+            inputlog.innerText = "Last Input: ArrowLeft";
+            console.log("ArrowLeft");
+            break;
+        case "ArrowRight":
+            iconX += 1 * speed;
+            
+            drawIcon(image);
+            //test feedback
+            inputlog.innerText = "Last Input: ArrowRight";
+            console.log("ArrowRight");
+            break;
+        default:
+            //test feedback
+            inputlog.innertext = `${key}`;
+            console.log(`${key}`);
+            break;
+    }
+}
+
+function drawIcon(img) {
+    //might add a param to change this
+    if (false) {
+        cnvs.clearRect(0, 0, canvas.width, canvas.height);
+        generateBorder();
+    }
+    //draw icon
+    ctx.drawImage(img, (canvas.width/2 + iconX - iconr), (canvas.height/2 + iconY - iconr));
+}
